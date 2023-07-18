5c5373b094d1023ebdf4e20a58c084980d6df0f5
Lluís Bosch <holalluis@gmail.com>
2023-07-19 00:10:10 +0200

    nou commit

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#git
+git add *
+git commit -am 'nou commit'
+git push
+
+#desplega la web al servidor
+# -h  human readable format
+# -P  mostra progrÃ©s
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync --rsync-path="sudo rsync" -h -P -vv -r . debian@51.91.250.220:/var/www/html/la_teva_vida_en_setmanes
